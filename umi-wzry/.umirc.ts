c8ea56352a6e13cfe841f4f8b7eb4b763440838c
lruler <1204312199@qq.com>
2021-10-14 18:20:30 +0800

    init dva

--- umi-wzry/.umirc.ts
+++ umi-wzry/.umirc.ts
@@ -1,11 +1,35 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  dva: {},
+  antd: {},
   nodeModulesTransform: {
     type: 'none',
   },
+  // 不能自动生成路径
   routes: [
-    { path: '/', component: '@/pages/index' },
+    {
+      path: '/',
+      component: '@/layouts/index',
+      routes: [
+        {
+          path: '/',
+          component: '@/pages/index'
+        },
+        {
+          path: '/hero',
+          component: '@/pages/hero'
+        },
+        {
+          path: '/item',
+          component: '@/pages/item'
+        },
+        {
+          path: '/summoner',
+          component: '@/pages/summoner'
+        },
+      ]
+    },
   ],
   fastRefresh: {},
 });

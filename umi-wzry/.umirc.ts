10e307f7b18677b61c2b2e1a033b229e42a421e5
lruler <1204312199@qq.com>
2021-10-17 17:33:18 +0800

    add zhushi

--- umi-wzry/.umirc.ts
+++ umi-wzry/.umirc.ts
@@ -33,10 +33,10 @@ export default defineConfig({
   // ],
   fastRefresh: {},
   "proxy": {
-    "/api/": {
-      "target": "https://pvp.qq.com/",
-      "changeOrigin": true,
-      "pathRewrite": { "^/api": "" }
+    "/api/": { //设置代理请求头，当访问到/api时就会触发代理
+      "target": "https://pvp.qq.com/", //代理访问的真实服务器地址
+      "changeOrigin": true, // 是否跨域请求地址
+      "pathRewrite": { "^/api": "" } // 是否重写请求地址，比如这里就是吧/api替换成空字符串
     }
   }
 });
